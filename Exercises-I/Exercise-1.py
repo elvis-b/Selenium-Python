a264de3a465b6e07fd82d7752e91cb56b56098c4
Elvis Bucatariu <Elvis.Bucatariu1@ness.com>
2025-04-15 07:55:20 +0300

    Remove comments

--- Exercises-I/Exercise-1.py
+++ Exercises-I/Exercise-1.py
@@ -16,22 +16,3 @@ def isPalindrome(string):
 while True: # I chose a while true loop so the user won't need to rerun the app every time they want to check a new string
     string = input("Add the string to check: ")
     isPalindrome(string)
-
-
-
-'''
-Exercise 5:
-
-Given a String find whether it is a valid 10-digit phone number.
-Number should be in format xxx-xxx-xxxx
-E.g 234-456-9999
-'''
-
-'''
-Exercise 6:
-
-Find whether a number is a Perfect number
-E.g A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself.
-For instance, 6 has divisors 1, 2 and 3 (excluding itself), and 1 + 2 + 3 = 6, so 6 is a perfect number.
-'''
-
